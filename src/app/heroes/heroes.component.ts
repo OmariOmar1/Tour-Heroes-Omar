b9b8460e447ad5198b0b761d5f4a434a18a703ce
OmarAlomari <omar.alomari@tickerchart.com>
2022-03-07 12:33:04 +0200

    creating the hero and using it to get data to hero.heroes.component
    	removed spec files

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES} from "./mock-heroes";
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -10,13 +10,18 @@ import { HEROES} from "./mock-heroes";
 
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes?: Hero[] ;
   selectedHero?: Hero;
 
-  constructor() { }
+  constructor(private heroService:HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes()
   }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHero();
+  }
+
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
