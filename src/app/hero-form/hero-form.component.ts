1de16f1cfdc7f7cb8f6c263c91ce8f54adf85221
OmarAlomari <omar.alomari@tickerchart.com>
2022-03-09 18:01:04 +0200

    fixed the hero-form.component to show the details of the hero directly

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from "../hero";
 
 @Component({
   selector: 'app-hero-form',
@@ -10,6 +11,12 @@ export class HeroFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
   }
+ power="";
+  powers=['Really smart','Really smart','Really fat','Really Passionate','Really strong','Really fast'];
+  model = new Hero(18,'dr Smartyboy',this.powers[0],'chicken tindies');
+  submitted=false;
+  title: string='';
 
 }
