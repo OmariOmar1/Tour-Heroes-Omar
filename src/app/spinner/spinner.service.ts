45033bfff80880fd2dd69aeb4835c1b726414dd9
OmarAlomari <omar.alomari@tickerchart.com>
2022-03-14 18:13:49 +0200

    created a spinner service and added Observable spinner that changes state when needed

---
+++ src/app/spinner/spinner.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpinnerService {
+private count = 0;
+private spinner$ = new BehaviorSubject<string>('')
+
+  constructor() { }
+
+  getSpinnerObserver(): Observable<string>{
+  return this.spinner$.asObservable()
+  }
+
+  requestStarted(){
+if(this.count == 1){
+  this.spinner$.next('start')
+  }
+}
+requestEnded() {
+  if (this.count === 0 || --this.count === 0) {
+    this.spinner$.next('stop');
+  }
+}
+
+resetSpinner(){
+  this.count=0;
+  this.spinner$.next('stop')
+}
+
+}
+
+
+
+
