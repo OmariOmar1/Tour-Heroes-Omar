7ce5b6032b7f3cd805b2af72dbbee67823ef8c58
OmarAlomari <omar.alomari@tickerchart.com>
2022-04-04 15:55:44 +0300

    fixed variable names in hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,13 +11,14 @@ import {SpinnerService} from "./spinner/spinner.service";
   providedIn: 'root'
 })
 export class HeroService {
-  private heroesUrlDetails =  'http://localhost/tourHeroesPHP/tour-heroes-php/hero-details.php';
-  private heroesUrlAll = 'http://localhost/tourHeroesPHP/tour-heroes-php/all-heroes.php';// URL to php json
-  private heroesUrlPost='http://localhost/tourHeroesPHP/tour-heroes-php/add-hero.php';
-  private heroUrlDelete='http://localhost/tourHeroesPHP/tour-heroes-php/delete-hero.php';
+  private heroDetailsUrl =  'http://localhost/tour-heroes-php/hero-details.php';
+  private heroListUrl = 'http://localhost/tour-heroes-php/list-heroes.php';
+  private addHeroUrl='http://localhost/tour-heroes-php/add-hero.php';
+  private heroDeleteUrl='http://localhost/tour-heroes-php/delete-hero.php';
 
-/*injecting http for getting heroes from client , message service for sending
- log messages every time we do something,spinner services to add spinner service every time we use the api */
+
+  /*injecting http for getting heroes from client , message service for sending
+   log messages every time we do something,spinner services to add spinner service every time we use the api */
   constructor(
     private messageServiceInHeroService: MessageService,
     private http: HttpClient,
@@ -25,17 +26,9 @@ export class HeroService {
 
   ){}
 
-  httpOptionsPost = {
-    headers: new HttpHeaders(
-      {
-        'content-type':'multipart/form-data'
-      }
-    )
-  }
-
   getHeroes(): Observable<Hero[]> {
     this.log("going to the http client");
-    let fetchedDataFrom: Observable<Hero[]> = this.http.get<Hero[]>(this.heroesUrlAll);
+    let fetchedDataFrom: Observable<Hero[]> = this.http.get<Hero[]>(this.heroListUrl);
     this.spinnerService.requestStarted()
     return fetchedDataFrom.pipe(
         catchError(this.handleError<Hero[]>(`getHeroes`, [])),
@@ -46,7 +39,7 @@ export class HeroService {
   /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
     this.spinnerService.requestStarted()
-    const url = `${this.heroesUrlDetails}/?HeroId=${id}`;
+    const url = `${this.heroDetailsUrl}?HeroId=${id}`;
     return this.http.get<Hero>(url).pipe(
       tap(_ => this.log(`fetched hero id=${id}`)),
       tap(_ => this.spinnerService.requestEnded()),
@@ -55,7 +48,7 @@ export class HeroService {
   }
 
   updateHero(hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrlAll, hero).pipe(
+    return this.http.put<any>(this.heroListUrl, hero).pipe(
       tap(_ => this.log(`updated hero id=${hero.HeroId}`)),
       catchError(this.handleError<any>('updateHero'))
     );
@@ -63,20 +56,14 @@ export class HeroService {
 
   /** POST: add a new hero to the server */
   addHero(hero: Hero): Observable<Hero> {
-    return this.http.post<Hero>(this.heroesUrlPost, hero).pipe(
-      tap(_=>console.log("Hero added"))
+    return this.http.post<Hero>(this.addHeroUrl, hero).pipe(
+      tap(_=> console.log('added hero'))
     );
   }
 
   /** DELETE: delete the hero from the server */
   deleteHero(id: number): Observable<Hero> {
-    const url = `${this.heroUrlDelete}`;
-const formdata = new FormData();
-formdata.append("HeroId",id.toString());
-    return this.http.post<any>(url,formdata).pipe(
-      tap(_ => this.log(`deleted hero id=${id}`)),
-      catchError(this.handleError<Hero>('deleteHero'))
-    );
+    return this.http.post<Hero>(this.heroDeleteUrl,id);
   }
 
   private log(message: string) {
@@ -98,7 +85,7 @@ formdata.append("HeroId",id.toString());
       // if not search term, return empty hero array.
       return of([]);
     }
-    return this.http.get<Hero[]>(`${this.heroesUrlAll}/?name=${term}`).pipe(
+    return this.http.get<Hero[]>(`${this.heroListUrl}/?name=${term}`).pipe(
       tap(x => x.length ?
         this.log(`found heroes matching "${term}"`) : this.log(`no heroes matching "${term}"`)),
       catchError(this.handleError<Hero[]>('searchHeroes', []))
